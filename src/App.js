7192d41eb5e3983cb875e7e0c179d677853e6ae1
Silvestre Barajas <LFS19110806@purisima.tecnm.mx>
2023-04-14 09:45:45 -0600

    Se agrego el Proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
+import ListaTareas from './componentes/ListaTareas';
 
-function App() {
+const App = () => {
+
+  const tareasGuardadas = 
+    localStorage.getItem('tareas') ? 
+    JSON.parse(localStorage.getItem('tareas')) : []; 
+
+  const [tareas, cambiarTareas] = useState(tareasGuardadas);
+   
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas]);
+
+  let configMostrarCompletadas = '';
+  if(localStorage.getItem('mostrarCompletadas') === null){
+    configMostrarCompletadas = true;
+  }else {
+   configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+  }
+
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+
+  useEffect(() => {
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+  }, [mostrarCompletadas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+        <Header 
+          mostrarCompletadas={mostrarCompletadas} 
+          cambiarMostrarCompletadas={cambiarMostrarCompletadas}
+        />
+        <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
+        <ListaTareas 
+          tareas={tareas} 
+          cambiarTareas={cambiarTareas}
+          mostrarCompletadas={mostrarCompletadas}
+         />
     </div>
   );
 }
